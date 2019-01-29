e5013fc5a6265e8a96383e30d87ed25993dd2461
Alexandr Sarandi <asarandi@student.42.us.org>
2019-01-29 08:57:04 -0800

    initial commit, able to draw triangles with tkinter and get color from png file

---
+++ img1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import cv2
+
+img = cv2.imread('IMG_0019.PNG')
+
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
